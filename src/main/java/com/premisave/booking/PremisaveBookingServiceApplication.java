7f1f2f3b5da525a54235e87e7d8061f6f436317e
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-23 00:11:37 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/premisave/booking/PremisaveBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.premisave.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremisaveBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremisaveBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premisave/booking/PremisaveBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premisave.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremisaveBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
